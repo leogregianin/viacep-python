d0fca42edc9589bd340ea96c29e9e16cc85c6ab1
leogregianin@gmail.com <leogregianin@gmail.com>
2017-08-31 17:48:00 -0400

    added pypi package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='viacep',
+      version='1.0',
+      description='Consulta CEP pelo webservice do ViaCEP.com.br',
+      author='Leonardo Gregianin',
+      author_email='leogregianin@gmail.com',
+      url='https://github.com/leogregianin/viacep-python',
+     )
