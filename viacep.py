762d4f2334d4346aeaa53b2f4e041f06ef5a23b0
leogregianin@gmail.com <leogregianin@gmail.com>
2017-09-06 08:40:44 -0400

    include Exceptions

--- viacep.py
+++ viacep.py
@@ -3,14 +3,31 @@
 
 import json
 import requests
+from sys import exit
 
 class ViaCEP:
 
-    def __init__(self, cep):
-        self.cep = cep
+	def __init__(self, cep):
+		self.cep = cep
 
-    def getDadosCEP(self):
-    	url_api = ('http://www.viacep.com.br/ws/%s/json' % self.cep)
-    	req = requests.get(url_api)
-    	dados_json = json.loads(req.text)
-    	return dados_json
+	def getDadosCEP(self):
+		url_api = ('http://www.viacep.com.br/ws/%s/json' % self.cep)
+		try:
+			req = requests.get(url_api)
+			if req.status_code == 200:
+				dados_json = json.loads(req.text)
+				return dados_json
+			else:
+				exit("Status code error: %s" % req.status_code)
+		except requests.exceptions.Timeout as err:
+			print(err)
+			exit(1)
+		except requests.exceptions.TooManyRedirects as err:
+			print(err)
+			exit(1)
+		except requests.exceptions.HTTPError as err:
+			print(err)
+			exit(1)
+		except requests.exceptions.RequestException as err:
+			print(err)
+			exit(1)
